2f451069627b429488eff3851dbe493026f613e1
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-10-08 17:13:26 -0500

    Fix player height

--- pages/_app.js
+++ pages/_app.js
@@ -6,27 +6,26 @@ import AppState from '../context/app-state';
 
 import NavBar from '../components/nav-bar';
 import Footer from '../components/footer';
-import router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel='icon' href='/favicon.png' />
-        <link
-          rel='stylesheet'
-          href='https://use.fontawesome.com/releases/v5.15.4/css/all.css'
-          integrity='sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm'
-          crossOrigin='anonymous'
-        />
-      </Head>
-      <AppState>
-        <NavBar />
-        <AnimateSharedLayout exitBeforeEnter>
+      <AnimateSharedLayout>
+        <Head>
+          <link rel='icon' href='/favicon.png' />
+          <link
+            rel='stylesheet'
+            href='https://use.fontawesome.com/releases/v5.15.4/css/all.css'
+            integrity='sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm'
+            crossOrigin='anonymous'
+          />
+        </Head>
+        <AppState>
+          <NavBar />
           <Component {...pageProps} />
-        </AnimateSharedLayout>
-        <Footer />
-      </AppState>
+          <Footer />
+        </AppState>
+      </AnimateSharedLayout>
     </>
   );
 }
